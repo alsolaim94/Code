39ba4e3e938534cf65159fe85acdf08482424f7e
mbutera11 <michael.butera11@gmail.com>
2018-01-31 19:38:33 -0600

    Implemented the Forgot Password Functionality
    On the log in screen you can now press forget password. It will ask for the email of the account. If it finds the email is in our database, it will send an email with a link to a reset. I will get to that a different time. PHPMailer-master is what allows emails to be sent from our akari.test496@gmail.com

---
+++ Mail_Functions.php
@@ -0,0 +1,27 @@
+ <?php
+
+require "PHPMailer-master/PHPMailerAutoload.php";
+
+
+function getMailObject() {
+    
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->SMTPSecure = 'ssl';
+    $mail->SMTPAuth = true;
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 465;
+    $mail->Username = 'akari.test496@gmail.com'; 
+    $mail->Password = 'Akari12345';
+    
+    $mail->setFrom('akari.test496'); 
+    
+    
+    return $mail;
+
+    
+    
+    
+}
+
+?>
