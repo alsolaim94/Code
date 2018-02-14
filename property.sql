8215b1217f09c49858d1e76140901d2e209f91b6
Firas Alsolaim <f.alsolaim@gmail.com>
2018-02-14 12:36:36 -0600

    pro

--- property.sql
+++ property.sql
@@ -1,3 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Feb 14, 2018 at 07:35 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akari`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
 
 CREATE TABLE `property` (
   `proprtyID` int(11) NOT NULL,
@@ -20,4 +49,37 @@ CREATE TABLE `property` (
   `contraction` year(4) DEFAULT NULL,
   `problem` text,
   `note` text
-) 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `property`
+--
+
+INSERT INTO `property` (`proprtyID`, `email`, `prooertyName`, `country`, `address`, `city`, `state`, `zipcode`, `phone`, `type`, `size`, `bedroom`, `bathroom`, `extra`, `lease`, `price`, `availability`, `contraction`, `problem`, `note`) VALUES
+(67, 'f.alsolaim@gmail.com', 'ttttttt', 'ttttt', 'ttttt', 'tttt', 'ttt', 'tt', '4567890', 'residential', 78, 87, 878, 'no', 'monthly', 45678, '2018-11-11', 2010, 'jj', NULL),
+(68, 'f.alsolaim@gmail.com', 'ttttttt', 'ttttt', 'ttttt', 'tttt', 'ttt', 'tt', '4567890', 'residential', 78, 87, 878, 'no', 'monthly', 45678, '2018-11-11', 2010, 'jj', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `property`
+--
+ALTER TABLE `property`
+  ADD PRIMARY KEY (`proprtyID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `property`
+--
+ALTER TABLE `property`
+  MODIFY `proprtyID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
